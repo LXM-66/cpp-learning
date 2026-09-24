bc2ff0b6f1f672ff034369c31ae3e8d0e223975e
LXM-66 <3131658952@qq.com>
2026-09-24 16:43:08 +0800

    Day 01: verify hello output, fill in notes

--- code/day01/hello.cpp
+++ code/day01/hello.cpp
@@ -2,9 +2,10 @@
 using namespace std;
 
 int main() {
-    // cout << "李林峻" << endl;
-    // cout<<"生物科学"<<endl;
-    // cout<<"202340470329"<<endl;
-    
+    cout << "李林峻" << endl;
+    cout<<"生物科学"<<endl;
+    cout<<"202340470329"<<endl;
+
     return 0;
 }
+
